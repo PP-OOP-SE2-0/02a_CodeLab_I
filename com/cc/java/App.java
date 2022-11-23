e3ce5d953c21db75a025e54a36671dd5bd7ce322
Sabine Eitel <Seit201@aol.com>
2022-11-23 16:28:43 +0100

    Lösung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,8 +4,14 @@ public class App {
     
     public static void main(String[] args) {
      
-    }
+        Konto konto1 = new Konto(10000);
+        Konto konto2 = new Konto(10000);
+        Konto konto3 = new Konto(10000);
 
+        output(String.valueOf(konto1.getKontostand()*2));
+        output(String.valueOf(konto2.getKontostand()*3));
+        output(String.valueOf(konto3.getKontostand()*10));
+    }
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
