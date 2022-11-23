c961aebd5cfaf669aa12baa52d59d07c393f1716
Sabine Eitel <Seit201@aol.com>
2022-11-23 15:57:54 +0100

    KontoV1

--- Konto.java
+++ Konto.java
@@ -1,3 +1,18 @@
-public class Konto {
-    
+package com.cc.java;
+
+public class Konto { 
+
+    private int kontostand;
+      
+    public Konto(int kontostand) 
+    {
+        this.kontostand = kontostand;
+    }
+    public int getKontostand() {
+        return kontostand; 
+    }
+    public void setKontostand(int kontostand) 
+    {
+        this.kontostand = kontostand;
+    }
 }
